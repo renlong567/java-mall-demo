d62f3ab97e6121010f1459597fb40d4db70c5df0
renlong <renlong@hhb123.cn>
2021-08-03 11:16:47 +0800

    剔除lombok

--- src/main/java/com/example/javademo/dto/UmsAdminLoginParam.java
+++ src/main/java/com/example/javademo/dto/UmsAdminLoginParam.java
@@ -1,8 +1,6 @@
 package com.example.javademo.dto;
 
 import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 import javax.validation.constraints.NotEmpty;
 
@@ -10,13 +8,57 @@ import javax.validation.constraints.NotEmpty;
  * 用户登录参数
  * Created by macro on 2018/4/26.
  */
-@Data
-@EqualsAndHashCode(callSuper = false)
 public class UmsAdminLoginParam {
     @NotEmpty
-    @ApiModelProperty(value = "用户名",required = true)
+    @ApiModelProperty(value = "用户名", required = true)
     private String username;
     @NotEmpty
-    @ApiModelProperty(value = "密码",required = true)
+    @ApiModelProperty(value = "密码", required = true)
     private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UmsAdminLoginParam{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    protected boolean canEqual(Object other) {
+        return other instanceof UmsAdminLoginParam;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UmsAdminLoginParam that = (UmsAdminLoginParam) o;
+
+        if (!username.equals(that.username)) return false;
+        return password.equals(that.password);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = username.hashCode();
+        result = 31 * result + password.hashCode();
+        return result;
+    }
 }
